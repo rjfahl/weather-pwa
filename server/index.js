e6e96865adee5b94564db9f75fa1bc8ddf9f324c
Robert Fahl <rfahl@apterainc.com>
2020-07-05 14:05:55 -0400

    Add Express Node Server to react app

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static("public"));
+
+app.listen(5000, () => {
+    console.log("server started on port 5000");
+});
