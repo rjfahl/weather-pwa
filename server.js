db8e8aa39435ec44ad2b7896836ddbfa4aebf865
Robert Fahl <rfahl@apterainc.com>
2020-07-05 21:47:34 -0400

    simplify server for serving the build folder

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
-const path = require("path");
 
-app.use(express.static(path.join(__dirname, "build")));
-app.use(express.static("public"));
+app.use(express.static("build"));
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("server started on port " + process.env.PORT || 5000);
