6d4b5ec59ef9462dbf7d9f7e7274fce53e8e1ab2
Robert Fahl <rfahl@apterainc.com>
2020-07-06 19:58:02 -0400

    update server to hopefully serve manifest

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 
-app.use(express.static("build"));
+app.use(express.static(path.join(__dirname, 'build/')))
+
+app.get('*', function(req, res) {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+})
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`server started on port ${process.env.PORT | 3000}`);
